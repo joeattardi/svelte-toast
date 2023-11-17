2b1c2532b900596256f3f6f56a8128999ca616ee
Joe Attardi <jattardi@gmail.com>
2023-11-16 21:13:31 -0500

    add Notification

--- src/Notification.svelte
+++ src/Notification.svelte
@@ -1,1 +1,15 @@
-<!-- In this file, you'll create the notification component. -->
+<script>
+  export let notification;
+</script>
+
+<li class="notification">
+  <div class="message">{notification.message}</div>
+</li>
+
+<style>
+  .notification {
+    background-color: #cccccc;
+    margin: 1em;
+    padding: 1em;
+  }
+</style>
