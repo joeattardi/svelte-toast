6fe8df74db859465d81ce4b1ed1942eb8b6baf64
Joe Attardi <jattardi@gmail.com>
2023-11-16 21:09:49 -0500

    add NotificationContainer

--- src/NotificationContainer.svelte
+++ src/NotificationContainer.svelte
@@ -1,1 +1,14 @@
-<!-- In this file, you'll create the notification container. -->
+<ul class="notification-container"></ul>
+
+<style>
+  .notification-container {
+    width: 350px;
+    padding: 1rem;
+    position: fixed;
+    top: 0;
+    right: 0;
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+</style>
