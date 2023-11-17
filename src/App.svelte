d38d398c520ac33def55683bdec8091b6cba6965
Joe Attardi <jattardi@gmail.com>
2023-11-16 21:47:41 -0500

    Render basic notification

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,14 @@
   import NotificationContainer from './NotificationContainer.svelte';
 
   let showNotification = false;
+
+  function handleClick() {
+    showNotification = true;
+  }
 </script>
 
 <NotificationContainer showNotification={showNotification} />
 <main>
   <h1>Svelte Toast</h1>
+  <button on:click={handleClick}>Show Notification</button>
 </main>
