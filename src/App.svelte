59935d43cccf68fd2c792989ba9b10601bc29bf4
Joe Attardi <jattardi@gmail.com>
2023-11-16 21:36:37 -0500

    Add NotificationContainer to App

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,10 @@
+<script>
+  import NotificationContainer from './NotificationContainer.svelte';
+
+  let showNotification = false;
+</script>
+
+<NotificationContainer showNotification={showNotification} />
 <main>
   <h1>Svelte Toast</h1>
 </main>
