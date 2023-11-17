6bac3dd886c8a898a2b097d78ad642b0969b3f3b
Joe Attardi <jattardi@gmail.com>
2023-11-16 22:11:12 -0500

    Add styles

--- src/Notification.svelte
+++ src/Notification.svelte
@@ -2,14 +2,26 @@
   export let notification;
 </script>
 
-<li class="notification">
+<li 
+  class="notification"
+  class:success={notification.type === 'success'}
+  class:error={notification.type === 'error'}
+>
   <div class="message">{notification.message}</div>
 </li>
 
 <style>
   .notification {
     background-color: #cccccc;
-    margin: 1em;
     padding: 1em;
+    margin: 1em;
+  }
+
+  .success {
+    background-color: #86efac;
+  }
+
+  .error {
+    background-color: #fca5a5;
   }
 </style>
