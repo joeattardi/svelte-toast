31b33449dde80cd9dddc6ac9c4c9785c590ecbac
Joe Attardi <jattardi@gmail.com>
2023-11-18 10:19:06 -0500

    Add buttons for notification styles

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,31 @@
   function handleClick() {
     showNotification({ message: 'Hello world!' });
   }
+
+  function handleClickSuccess() {
+    showNotification({ type: 'success', message: 'Success!' });
+  }
+
+  function handleClickError() {
+    showNotification({ type: 'error', message: 'An error occurred!' });
+  }
 </script>
 
 <NotificationContainer />
 <main>
   <h1>Svelte Toast</h1>
-  <button on:click={handleClick}>Show Notification</button>
+  <div class="buttons">
+    <button on:click={handleClick}>Show Notification</button>
+    <button on:click={handleClickSuccess}>Show Success</button>
+    <button on:click={handleClickError}>Show Error</button>
+  </div>
 </main>
+
+<style>
+  .buttons {
+    display: flex;
+    gap: 8px;
+    flex-direction: column;
+    width: 250px;
+  }
+</style>
